49b07a416463033484dab296577a97ce70b74a78
sondrein <sondrein@ifi.uio.no>
2025-03-25 23:00:24 +0100

    Update API call

--- app/src/main/java/no/uio/ifi/in2000/sondrein/ai_chat/data/repository/GeminiRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/ai_chat/data/repository/GeminiRepository.kt
@@ -3,8 +3,10 @@ package no.uio.ifi.in2000.sondrein.ai_chat.data.repository
 import no.uio.ifi.in2000.sondrein.ai_chat.data.datasource.GeminiDatasource
 import no.uio.ifi.in2000.sondrein.ai_chat.data.models.GeminiResponse
 
-class GeminiRepository(private val apiService: GeminiDatasource) {
+class GeminiRepository() {
+    private val datasource = GeminiDatasource()
+
     suspend fun getAnswer(question: String): GeminiResponse {
-        return apiService.askQuestion(question)
+        return datasource.askQuestion(question)
     }
 }
--- app/src/main/java/no/uio/ifi/in2000/sondrein/ai_chat/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/ai_chat/ui/home/HomeViewModel.kt
@@ -4,26 +4,32 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import no.uio.ifi.in2000.sondrein.ai_chat.data.datasource.GeminiDatasource
 import no.uio.ifi.in2000.sondrein.ai_chat.data.models.GeminiResponse
 import no.uio.ifi.in2000.sondrein.ai_chat.data.repository.GeminiRepository
 
 class HomeViewModel : ViewModel() {
-    private val repository = GeminiRepository(GeminiDatasource())
+    private val repository = GeminiRepository()
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState
 
     fun askQuestion(question: String) {
         viewModelScope.launch {
-            _uiState.value = UiState(isLoading = true)
+            _uiState.update {
+                it.copy(isLoading = true)
+            }
             when (val response = repository.getAnswer(question)) {
                 is GeminiResponse.Success -> {
-                    _uiState.value = UiState(response = response.text, isLoading = false)  // Oppdaterer med svar
+                    _uiState.update {
+                        it.copy(response = response.text, isLoading = false)
+                    }
                 }
                 is GeminiResponse.Error -> {
-                    _uiState.value = UiState(response = "Error: ${response.message}", isLoading = false)  // Håndterer feilmelding
+                    _uiState.update {
+                        it.copy(response = "Error: ${response.message}", isLoading = false)
+                    }
                 }
             }
         }
